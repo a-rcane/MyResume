1bcef7ef87b0a8a1e93661f284a84da9fc26ff92
a-rcane <sanchit2k15@gmail.com>
2022-02-20 02:51:12 +0530

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+module.exports = {
+    mode: "production",
+    entry: '.Resume/app/javascript/channels/index.js',
+    output: {
+        /*Webpack producing results*/
+        path: path.resolve(__dirname, "../src/dist"),
+        filename: "app-bundle.js"
+    }
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }
+      ]
+    }
+}
